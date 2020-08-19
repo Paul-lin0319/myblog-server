f5be3521a9deb48a2760364c605bb1f69e7a0fd5
林晓峰 <linxiaofeng@linxiaofengdeMacBook-Pro.local>
2020-08-19 15:29:21 +0800

    不使用框架代码

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const querystring = require('querystring');
 const { get, set } = require('./src/db/redis');
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
+const { access } = require('./src/utils/log');
 
 // session 数据
 // const SESSION_DATA = {};
@@ -39,6 +40,13 @@ const getPostData = (req) => {
 };
 
 const serverHandle = (req, res) => {
+  // 记录access log
+  access(
+    `${req.method} -- ${req.url} -- ${
+      req.headers['user-agent']
+    } -- ${Date.now()}`
+  );
+
   // 设置返回格式 JSON
   res.setHeader('Content-type', 'application/json');
 
--- src/utils/log.js
+++ src/utils/log.js
@@ -1,5 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-// 生成 write stream 
-function createWriteStream(fileName) { };
+// 写日志
+function writeLog(writeStream, log) {
+  writeStream.write(log + '\n'); // 关键代码
+}
+
+// 生成 write stream
+function createWriteStream(fileName) {
+  const fullFileName = path.join(__dirname, '../', '../', 'logs', fileName);
+  const writeStream = fs.createWriteStream(fullFileName, {
+    flags: 'a',
+  });
+  return writeStream;
+}
+
+// 写访问日志
+const accessWriteStream = createWriteStream('access.log');
+function access(log) {
+  writeLog(accessWriteStream, log);
+}
+
+// 写事件日志
+const eventWriteStream = createWriteStream('event.log');
+function event(log) {
+  writeLog(eventWriteStream, log);
+}
+
+// 写报错日志
+const errorWriteStream = createWriteStream('error.log');
+function error(log) {
+  writeLog(errorWriteStream, log);
+}
+
+module.exports = {
+  access,
+  event,
+  error,
+};
